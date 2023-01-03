5a775bf95b4eccb6023a428c82548516028258ff
abir <abir@Asma-Azim-Abir>
2023-01-03 17:22:09 +0600

    Second project

---
+++ PracticingOOP/src/Test/HallMember.java
@@ -0,0 +1,21 @@
+package Test;
+
+public class HallMember {
+
+	void showName1()
+	{
+		System.out.println("Room #W-313: Arafat Hossain");
+		
+	}
+	void showName2()
+	{
+		System.out.println("Room #W-314: Md. Masud");
+		
+	}
+	void showName3()
+	{
+		System.out.println("Room #W-315: Md. Srijon");
+		
+	}
+	
+}
---
+++ PracticingOOP/src/Test/HallMemberInfo.java
@@ -0,0 +1,12 @@
+package Test;
+
+public class HallMemberInfo {
+
+	public static void main(String[] args) {
+		HallMember hm = new HallMember();
+		
+		hm.showName1();
+		hm.showName2();
+		hm.showName3();
+	}
+}
